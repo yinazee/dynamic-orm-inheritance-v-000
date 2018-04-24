e99b95731f212b0c1b742d61c1736f354ffb9b2d
Yina Zhu <yinagit@gmail.com>
2018-04-24 13:54:28 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,9 +48,15 @@ class InteractiveRecord
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
+
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
+  end
+
+  def self.find_by(attribute_hash)
+    binding.pry
+      sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
   end
 
 end
