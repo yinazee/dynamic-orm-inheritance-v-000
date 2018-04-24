0d20e47cdf9a65d93e8750935d19519e6c27dcff
Yina Zhu <yinagit@gmail.com>
2018-04-24 13:21:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,10 @@
 require_relative "./interactive_record.rb"
-
+require 'pry'
 class Song < InteractiveRecord
 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
+    binding.pry
   end
 
 end
